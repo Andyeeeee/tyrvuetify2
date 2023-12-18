0d99bc89fc51447709871e40fc5987b0196db470
Andyeeeee <10514f074@gmail.com>
2023-12-18 17:06:59 +0800

    ss

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
     children: [
       {
         path: '',
-        // name: 'Home',
+        name: 'Home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
@@ -19,7 +19,7 @@ const routes = [
       },
       {
         path: 'aboutme',
-        // name: 'Home',
+        name: 'AboutMe',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
