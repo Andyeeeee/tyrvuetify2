812c3d9e42510cc0fb8f3c06024843701317e38f
Andyeeeee <10514f074@gmail.com>
2023-12-18 16:56:15 +0800

    01

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,14 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+      },
+      {
+        path: 'aboutme',
+        name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "AboutMe" */ '@/views/AboutMe.vue')
       }
     ]
   }
