563d14e283d7ddb107bf6768432dfbc40450c0ee
Andyeeeee <10514f074@gmail.com>
2023-12-18 18:29:50 +0800

    sd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="HomeView">
     <v-container>
-      <v-row>
-        <!-- <v-col class="v-col-12 v-col-sm-6 v-col-lg-4"> -->
-        <!-- <v-btn :title="item.title" :text="item.text" :icon="item.icon" :to="item.to">
+      <v-row sm="12" class="text-center">
+        <v-col sm="6" md="4" v-for="item in items" :key="item.to">
+          <v-btn :title="item.title" :text="item.text" :icon="item.icon" :to="item.to">
           </v-btn>
-          <br> -->
-        <!-- {{ item.title }} -->
-        <!-- </v-col> -->
-        content
+          <br>
+          {{ item.title }}
+        </v-col>
       </v-row>
+      content
     </v-container>
   </div>
 </template>
 <script setup>
-// import { computed } from 'vue'
-// const items = computed(() => {
-//   return [
-//     { to: 'aboutme', title: '關於我', icon: 'mdi-face-man', text: '關於我' },
-//     { to: 'aboutme', title: '我的作品', icon: 'mdi-book-open', text: '我的作品' },
-//     { to: 'aboutme', title: '我的技能', icon: 'mdi-hubspot', text: '我的技能' },
-//     { to: 'aboutme', title: '學習歷程', icon: 'mdi-book-open-page-variant', text: '學習歷程' },
-//     { to: 'aboutme', title: '我的經歷', icon: 'mdi-ski', text: '我的經歷' }
-//   ]
-// })
+import { computed } from 'vue'
+const items = computed(() => {
+  return [
+    { to: 'aboutme', title: '關於我', icon: 'mdi-face-man', text: '關於我' },
+    { to: 'aboutme', title: '我的作品', icon: 'mdi-book-open', text: '我的作品' },
+    { to: 'aboutme', title: '我的技能', icon: 'mdi-hubspot', text: '我的技能' },
+    { to: 'aboutme', title: '學習歷程', icon: 'mdi-book-open-page-variant', text: '學習歷程' },
+    { to: 'aboutme', title: '我的經歷', icon: 'mdi-ski', text: '我的經歷' }
+  ]
+})
 </script>
